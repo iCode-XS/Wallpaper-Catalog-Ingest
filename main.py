6c536c00fac62247261917ac4969e3120c43afcb
Shivam Kanwar <shivamkanwar701@gmail.com>
2026-05-18 00:38:03 +0530

    Catalog-Ingest | Version 0.3 Beta | Parsed the webpage where all the wallpapers are located and also cleaned and extracted the 'names' and 'prices' of the wallpapers

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ url = 'https://www.yorkwallcoverings.com/wallpaper-york'
 
 session = httpx.Client(headers=user_agents.chromium_linux, http2=True)
 
-
+@logger.catch
 def fetch_website(link, timeout):   # Fetching the website from the server
 
     try:
@@ -24,19 +24,21 @@ def fetch_website(link, timeout):   # Fetching the website from the server
         logger.info('Success! The website has been successfully fetched from the server...\n')
         logger.info(f'Target Site: {response.url}')
         logger.info(f'Status Code: {response.status_code}\n')
+        return response
 
     except httpx.HTTPStatusError as e:
         logger.error(f'HTTP Request Error Occured! Status Code: {e.response.status_code} | {e.response.url}')
-
+    
 
 cookies = {}
 
 
-def browser_cookies():  # This function looks and saves the cookies in a 'cookie.json' file - presented by a server
+def browser_cookies():  # This function looks and saves the cookies in a 'cookie.json' file - presented by the server
 
     if not os.path.exists('cookies.json'):  # This if statement is checking if 'cookie.json' file exists or not
+
         with open('cookies.json', 'w') as f:    # If it doesn't exist, it will save all the cookies in a 'cookie.json' file
-            json.dump(cookies, f, indent=4)     # If it does exist then this entire block will not run by python
+            json.dump(cookies, f, indent=4)
 
         logger.info(f'Cookies assigned to us: {len(session.cookies)}\n')
 
@@ -51,7 +53,7 @@ def browser_cookies():  # This function looks and saves the cookies in a 'cookie
 if os.path.exists('cookies.json'):  # This if statement is checking if a 'cookies.json' file exists or not
 
     with open('cookies.json', 'r') as f:    # If that file exists then it will reuse the cookies
-        reuse_cookies = json.load(f)        # If not then this will not run by python
+        reuse_cookies = json.load(f)
         session.cookies.update(httpx.Cookies(reuse_cookies))
 
     logger.info('"Cookies.json" file exists! Reloading cookies...\n')
@@ -59,6 +61,36 @@ if os.path.exists('cookies.json'):  # This if statement is checking if a 'cookie
         logger.info(f'{x}: {y}')
     logger.info('\n')
 
-fetch_website(url, 120)
+
+@logger.catch
+def parsing_site(response):
+    try:
+        soup = BeautifulSoup(response, 'lxml')
+        logger.info('Parsing has been successful!')
+        return soup
+
+    except Exception as e:
+        logger.critical('The parsing of HTML document has been failed... The script will not be able to hold without this function!', e)
+
+@logger.catch
+def main_page_ingest(soup):
+    try:
+        item_box = soup.find_all('div', class_='item-box')
+        for x in item_box:
+            title_container = x.find('h3', class_='product-title')
+            title = title_container.find('a').text
+            price_container = x.find('div', class_='prices')
+            price = price_container.find('span', class_='price actual-price avoid-wrap').text
+            actual_price = price.strip('Actual Price:USD ')
+            print('Name:', title)
+            print('Price:', actual_price)
+            print('\n')
+
+    except Exception as e:
+        logger.error('There is a problem in retrieving data from main_page!', e)
+
+response = fetch_website(url, 120)
 browser_cookies()
+soup = parsing_site(response)
+wallpaper = main_page_ingest(soup)
 session.close()
